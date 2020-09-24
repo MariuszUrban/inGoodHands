d1ee2ba4993117539a82f5bc5e430d3c5f522816
Mariusz <urban.mariusz@icloud.com>
2020-09-24 15:41:25 +0200

    done

---
+++ good-hands/src/components/home/ThreeColumns.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function ThreeColumns() {
+  return (
+    <div className="three-columns-container col-lg-12">
+      <div className="column col-lg-4">
+        <div className="content-box">
+          <h1> liczba </h1>
+          <h2>ODDANYCH WORKÓW</h2>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur velit dolores, officiis illum necessitatibus provident.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="column col-lg-4">
+        <div className="content-box">
+          <h1> liczba </h1>
+          <h2>WSPARTYCH ORGANIZACJI</h2>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur velit dolores, officiis illum necessitatibus provident.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="column col-lg-4">
+        <div className="content-box">
+          <h1> liczba </h1>
+          <h2>ZORGANIZOWANYCH ZBIÓREK</h2>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur velit dolores, officiis illum necessitatibus provident.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- good-hands/src/components/home/index.jsx
+++ good-hands/src/components/home/index.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import LoginNav from './LoginNav';
-import MenuNav from './MenuNav'
-import PhotoContainer from './PhotoContainer';
-import ButtonsContainer from './ButtonsContainer';
-import '../../scss/components/_home.scss';
+import React from "react";
+import LoginNav from "./LoginNav";
+import MenuNav from "./MenuNav";
+import PhotoContainer from "./PhotoContainer";
+import ButtonsContainer from "./ButtonsContainer";
+import ThreeColumns from './ThreeColumns'
+import "../../scss/components/_home.scss";
 
 export default function Home() {
-    return (
-        <div className='home-container col-lg-12'>
-            <div className='box col-lg-6'>
-                <PhotoContainer />
+  return (
+    <main>
+      <section id="home-page">
+        <div className="home-container col-lg-12">
+          <div className="box col-lg-6">
+            <PhotoContainer />
+          </div>
+          <div className="box col-lg-6">
+            <div className="nav-row">
+              <LoginNav />
+              <MenuNav />
             </div>
-            <div className='box col-lg-6'>
-                <div className='nav-row'>
-                <LoginNav />
-                <MenuNav />
-                </div>
-                <div className='buttons-row'>
-                 <ButtonsContainer />
-                </div>
+            <div className="buttons-row">
+              <ButtonsContainer />
             </div>
+          </div>
         </div>
-    )
+      </section>
+      <section id='info-columns'>
+        <ThreeColumns />
+      </section>
+    </main>
+  );
 }
